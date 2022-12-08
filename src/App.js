7d574323e6e302c4b5f6bb40125f3b6d7117cbea
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-12-08 18:45:29 -0500

    connected to backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+
+  let [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch(' http://127.0.0.1:5000/users', {
+      mode: 'cors'
+    })
+      .then(res => {
+        return res.json()
+      })
+      .then(user => {
+        console.log(user)
+        return setUser(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }, [])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/users', {
+  //     mode: 'no-cors'
+  //   })
+
+  //     .then(data => {
+  //       console.log(data)
+  //     })
+  // })
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/users', {
+  //     mode: 'no-cors'
+  //   }, {
+  //     method: 'GET',
+  //     headers: {
+  //       accept: 'application/json'
+  //     }
+  //   })
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(data => {
+  //       console.log(data)
+  //     })
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React and flask</h1>
       </header>
     </div>
   );
